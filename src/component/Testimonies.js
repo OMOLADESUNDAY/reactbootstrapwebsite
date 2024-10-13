60e4edd9a58c0bfa7497e579dbad3d7fec39f786
spyer <sundayomoladee11@gmail.com>
2024-10-13 09:37:15 +0100

    adding responsiveness to testionials with react-slick

--- src/component/Testimonies.js
+++ src/component/Testimonies.js
@@ -9,16 +9,42 @@ import TestimonyItem from './TestimonyItem';
 
 const Testimonies = () => {
   var settings = {
-    autoplay:true,
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   return (
     <div >
-      <h2>Our Testimonials</h2>
+      <h2 className='majorTestimonyText'>Our Testimonials</h2>
     <Slider className='testimonySlider' {...settings}>
         {TestimonyData.map((singleTestimonyItem)=>{
         return <TestimonyItem  key={singleTestimonyItem.id} singleTestimonyItem={singleTestimonyItem} />
