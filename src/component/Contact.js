55969d499611d80847d7176a680ac041ab4b508f
spyer <sundayomoladee11@gmail.com>
2024-10-14 15:24:46 +0100

    adding styles to the contact page

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -4,22 +4,23 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './contact.css'
+import Button from 'react-bootstrap/Button';
 const Contact = () => {
   return (
     <section className='contactContainer'>
       <h2 className='majorTestimonyText'>Contact Us</h2>
       <Form>
         <Container fluid> 
-    <Row>
-      <Col>
+      <Row>
+        <Col sm={12} md={6}>
         <Form.Group className="mb-3" controlId="firstname">
-          <Form.Label>Firstname</Form.Label>
+          <Form.Label className='formlabel'>Firstname</Form.Label>
           <Form.Control type="text" placeholder="firstname" />
       </Form.Group>
       </Col>
-      <Col>
+      <Col xs={12} md={6}>
       <Form.Group className="mb-3" controlId="lastname">
-          <Form.Label>Lastname</Form.Label>
+          <Form.Label className='formlabel'>Lastname</Form.Label>
           <Form.Control type="text" placeholder="lastname" />
       </Form.Group>
       </Col>
@@ -27,17 +28,21 @@ const Contact = () => {
     <Row>
       <Col xs={12} md={6} >
         <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email</Form.Label>
+          <Form.Label className='formlabel'>Email</Form.Label>
           <Form.Control type="email" placeholder="email" />
       </Form.Group>
       </Col>
     </Row>
     <Row>
     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Message</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Label className='formlabel'>Message</Form.Label>
+        <Form.Control as="textarea" rows={5} />
       </Form.Group>
     </Row>
+    <div className='submitbtn'>
+    <Button  variant="dark">Submit</Button>{' '}
+    </div>
+    
   </Container>    
     </Form>
     </section>
