95838529f32e97a9cd7cc8d30f9efc3c9c45bae9
spyer <sundayomoladee11@gmail.com>
2024-10-16 15:15:51 +0100

    addition of social icon to the footer links

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,6 +5,10 @@ import Col from 'react-bootstrap/Col';
 import { Nav, Navbar } from 'react-bootstrap';
 import Logo from "../images/webro logo.png"
 import './footer.css'
+import { FaFacebook } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div className='footerContainer'>
@@ -17,29 +21,24 @@ const Footer = () => {
                 </Col>
                 <Col lg={9}>
                 <Row className='footernavlink'>
-                    <Col> 
-                      <Nav.Link  href="#home">Home</Nav.Link>
-                      <Nav.Link  href="#link">Link</Nav.Link>
-                      <Nav.Link href="#home">Home</Nav.Link>
-                      <Nav.Link href="#link">Link</Nav.Link>
-                    </Col>
+                   
                     <Col>
                     <Nav.Link href="#home">Home</Nav.Link>
-                      <Nav.Link href="#link">Link</Nav.Link>
-                      <Nav.Link href="#home">Home</Nav.Link>
-                      <Nav.Link href="#link">Link</Nav.Link>
+                      <Nav.Link href="#about">About</Nav.Link>
+                      <Nav.Link href="#service">Service</Nav.Link>
+                      <Nav.Link href="#works">Works</Nav.Link>
                     </Col>
                     <Col>
-                    <Nav.Link href="#home">Home</Nav.Link>
-                      <Nav.Link href="#link">Link</Nav.Link>
-                      <Nav.Link href="#home">Home</Nav.Link>
-                      <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav.Link href="#works">Consultation</Nav.Link>
+                      <Nav.Link href="#contact">Contact us</Nav.Link>
+                      <Nav.Link href="#experience">Experience</Nav.Link>
+                      <Nav.Link href="#contact">live Chat</Nav.Link>
                     </Col>
                     <Col>
-                    <Nav.Link href="#home">Home</Nav.Link>
-                      <Nav.Link href="#link">Link</Nav.Link>
-                      <Nav.Link href="#home">Home</Nav.Link>
-                      <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav.Link href="#home"><FaFacebook /></Nav.Link>
+                      <Nav.Link href="#link"><IoLogoWhatsapp /></Nav.Link>
+                      <Nav.Link href="#home"><FaLinkedin /></Nav.Link>
+                      <Nav.Link href="#link"><FaSquareInstagram /></Nav.Link>
                     </Col>
                 </Row>
                 </Col>
